52e54997ecf57d58f84c2f4603bfce02073199b5
Garrett Breach <breachg1992@gmail.com>
2020-10-22 21:33:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop(2)
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift(0)
+}
+
+function appendKitten(name) {
+  kittens = [...kittens, name]
+  return kittens
+}
